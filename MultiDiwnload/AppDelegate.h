4ccaca19d06737ba0a13a309ed9364b461da3330
Gagan5278 <gagan5278@gmail.com>
2014-04-10 12:44:38 +0530

    Initial Commit

---
+++ MultiDiwnload/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MultiDiwnload
+//
+//  Created by Gagan on 10/04/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MultiDiwnload/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultiDiwnload
+//
+//  Created by Gagan on 10/04/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
